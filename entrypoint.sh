b80c625139f237dc9a1b19deace99b996d298c52
Daisuke Fujita <dtanshi45@gmail.com>
2016-02-18 10:35:22 +0900

    Set base domain from environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,10 @@ if [ ! -d /home/git/.ssh ]; then
   chown -R git /home/git/.ssh
 fi
 
+if [ -n "$PAUS_BASE_DOMAIN" ]; then
+  echo $PAUS_BASE_DOMAIN > /base-domain
+else
+  echo "example.com" > /base-domain
+fi
+
 exec $@
