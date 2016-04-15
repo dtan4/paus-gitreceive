28e966a964ada5e3a409d2bd35336afdaa2a3abf
Daisuke Fujita <dtanshi45@gmail.com>
2016-04-15 14:03:15 +0900

    Unpack received tar archive

--- receiver/main.go
+++ receiver/main.go
@@ -1,34 +1,100 @@
 package main
 
 import (
+	"archive/tar"
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 const (
-	DefaultDockerHost   = "tcp://localhost:2375"
-	DefaultEtcdEndpoint = "http://localhost:2379"
+	DefaultDockerHost    = "tcp://localhost:2375"
+	DefaultEtcdEndpoint  = "http://localhost:2379"
+	DefaultRepositoryDir = "/repos"
 )
 
+func unpackReceivedFiles(repositoryDir, username, projectName string, stdin io.Reader) (string, error) {
+	repositoryPath := filepath.Join(repositoryDir, username, projectName)
+
+	if err := os.MkdirAll(repositoryPath, 0777); err != nil {
+		return "", err
+	}
+
+	reader := tar.NewReader(stdin)
+
+	for {
+		header, err := reader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return "", err
+		}
+
+		buffer := new(bytes.Buffer)
+		outPath := filepath.Join(repositoryPath, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if _, err = os.Stat(outPath); err != nil {
+				os.MkdirAll(outPath, 0755)
+			}
+
+		case tar.TypeReg, tar.TypeRegA:
+			if _, err = io.Copy(buffer, reader); err != nil {
+				return "", err
+			}
+
+			if err = ioutil.WriteFile(outPath, buffer.Bytes(), os.FileMode(header.Mode)); err != nil {
+				return "", err
+			}
+		}
+	}
+
+	return repositoryPath, nil
+}
+
 func main() {
-	baseDir, _ := os.Getwd()
+	workingDir, _ := os.Getwd()
+
 	dockerHost := os.Getenv("DOCKER_HOST")
-	etcdEndpoint := os.Getenv("ETCD_ENDPOINT")
 
 	if dockerHost == "" {
 		dockerHost = DefaultDockerHost
 	}
 
+	etcdEndpoint := os.Getenv("ETCD_ENDPOINT")
+
 	if etcdEndpoint == "" {
 		etcdEndpoint = DefaultEtcdEndpoint
 	}
 
+	repositoryDir := os.Getenv("REPOSITORY_DIR")
+
+	if repositoryDir == "" {
+		repositoryDir = DefaultRepositoryDir
+	}
+
 	commitMetadata := NewCommitMetadataFromArgs(os.Args[1:])
 
-	fmt.Println(baseDir)
+	fmt.Println(workingDir)
 	fmt.Println(commitMetadata.Repository)
 	fmt.Println(commitMetadata.Revision)
 	fmt.Println(commitMetadata.Username)
 	fmt.Println(commitMetadata.AppName)
 	fmt.Println(commitMetadata.ProjectName)
+
+	repositoryPath, err := unpackReceivedFiles(repositoryDir, commitMetadata.Username, commitMetadata.AppName, os.Stdin)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(repositoryPath)
 }
