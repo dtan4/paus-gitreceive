7c3af6d60eeba76c45bcd8783af957155d73ddda
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-17 11:58:20 +0900

    Use Compose instead of ComposeFile

--- receiver/model/compose.go
+++ receiver/model/compose.go
@@ -26,17 +26,18 @@ var (
 )
 
 type Compose struct {
-	dockerHost      string
-	composeFilePath string
-	projectName     string
-	project         *project.Project
+	ComposeFilePath string
+	ProjectName     string
+
+	dockerHost string
+	project    *project.Project
 }
 
 type ComposeConfig struct {
-	version  string                           `yaml:"version,omitempty"`
-	services map[string]*config.ServiceConfig `yaml:"services,omitempty"`
-	volumes  map[string]*config.VolumeConfig  `yaml:"volumes,omitempty"`
-	networks map[string]*config.NetworkConfig `yaml:"networks,omitempty"`
+	Version  string                           `yaml:"version,omitempty"`
+	Services map[string]*config.ServiceConfig `yaml:"services,omitempty"`
+	Volumes  map[string]*config.VolumeConfig  `yaml:"volumes,omitempty"`
+	Networks map[string]*config.NetworkConfig `yaml:"networks,omitempty"`
 }
 
 func NewCompose(dockerHost, composeFilePath, projectName string) (*Compose, error) {
@@ -50,31 +51,31 @@ func NewCompose(dockerHost, composeFilePath, projectName string) (*Compose, erro
 	}
 
 	return &Compose{
-		dockerHost,
-		composeFilePath,
-		projectName,
-		prj,
+		ComposeFilePath: composeFilePath,
+		ProjectName:     projectName,
+		dockerHost:      dockerHost,
+		project:         prj,
 	}, nil
 }
 
 func (c *Compose) Build() error {
-	cmd := exec.Command("docker-compose", "-f", c.composeFilePath, "-p", c.projectName, "build")
+	cmd := exec.Command("docker-compose", "-f", c.ComposeFilePath, "-p", c.ProjectName, "build")
 	cmd.Env = append(os.Environ(), "DOCKER_HOST="+c.dockerHost)
 
 	if err := util.RunCommand(cmd); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Failed to build Docker Compose project. projectName: %s", c.projectName))
+		return errors.Wrap(err, fmt.Sprintf("Failed to build Docker Compose project. projectName: %s", c.ProjectName))
 	}
 
 	return nil
 }
 
 func (c *Compose) GetContainerID(service string) (string, error) {
-	cmd := exec.Command("docker-compose", "-f", c.composeFilePath, "-p", c.projectName, "ps", "-q", service)
+	cmd := exec.Command("docker-compose", "-f", c.ComposeFilePath, "-p", c.ProjectName, "ps", "-q", service)
 	cmd.Env = append(os.Environ(), "DOCKER_HOST="+c.dockerHost)
 	out, err := cmd.Output()
 
 	if err != nil {
-		return "", errors.Wrap(err, fmt.Sprintf("Failed to get container ID. projectName: %s, service: %s", c.projectName, service))
+		return "", errors.Wrap(err, fmt.Sprintf("Failed to get container ID. projectName: %s, service: %s", c.ProjectName, service))
 	}
 
 	return strings.Replace(string(out), "\n", "", -1), nil
@@ -105,11 +106,11 @@ func (c *Compose) InjectEnvironmentVariables(envs map[string]string) {
 }
 
 func (c *Compose) Pull() error {
-	cmd := exec.Command("docker-compose", "-f", c.composeFilePath, "-p", c.projectName, "pull")
+	cmd := exec.Command("docker-compose", "-f", c.ComposeFilePath, "-p", c.ProjectName, "pull")
 	cmd.Env = append(os.Environ(), "DOCKER_HOST="+c.dockerHost)
 
 	if err := util.RunCommand(cmd); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Failed to pull images in Docker Compose project. projectName: %s", c.projectName))
+		return errors.Wrap(err, fmt.Sprintf("Failed to pull images in Docker Compose project. projectName: %s", c.ProjectName))
 	}
 
 	return nil
@@ -142,8 +143,6 @@ func (c *Compose) RewritePortBindings() {
 }
 
 func (c *Compose) SaveAs(filePath string) error {
-	fmt.Println(filePath)
-
 	services := map[string]*config.ServiceConfig{}
 
 	for _, key := range c.project.ServiceConfigs.Keys() {
@@ -153,10 +152,10 @@ func (c *Compose) SaveAs(filePath string) error {
 	}
 
 	cfg := &ComposeConfig{
-		version:  "2",
-		services: services,
-		volumes:  c.project.VolumeConfigs,
-		networks: c.project.NetworkConfigs,
+		Version:  "2",
+		Services: services,
+		Volumes:  c.project.VolumeConfigs,
+		Networks: c.project.NetworkConfigs,
 	}
 
 	data, err := yaml.Marshal(cfg)
@@ -173,11 +172,11 @@ func (c *Compose) SaveAs(filePath string) error {
 }
 
 func (c *Compose) Up() error {
-	cmd := exec.Command("docker-compose", "-f", c.composeFilePath, "-p", c.projectName, "up", "-d")
+	cmd := exec.Command("docker-compose", "-f", c.ComposeFilePath, "-p", c.ProjectName, "up", "-d")
 	cmd.Env = append(os.Environ(), "DOCKER_HOST="+c.dockerHost)
 
 	if err := util.RunCommand(cmd); err != nil {
-		return errors.Wrap(err, fmt.Sprintf("Failed to start Docker Compose project. projectName: %s", c.projectName))
+		return errors.Wrap(err, fmt.Sprintf("Failed to start Docker Compose project. projectName: %s", c.ProjectName))
 	}
 
 	return nil
