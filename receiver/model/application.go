e637a669fd361bf2a7e65a558a12b01e8a3b732b
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-24 17:15:50 +0900

    Return appropriate error from application

--- receiver/model/application.go
+++ receiver/model/application.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -62,14 +61,14 @@ func (app *Application) BuildArgs(etcd *store.Etcd) (map[string]string, error) {
 	buildArgKeys, err := etcd.List(buildArgsKey, false)
 
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to get build arg keys.")
+		return nil, err
 	}
 
 	for _, key := range buildArgKeys {
 		value, err := etcd.Get(key)
 
 		if err != nil {
-			return nil, errors.Wrap(err, fmt.Sprintf("Failed to get build arg value. key: %s", key))
+			return nil, err
 		}
 
 		args[strings.Replace(key, buildArgsKey, "", 1)] = value
@@ -102,14 +101,14 @@ func (app *Application) EnvironmentVariables(etcd *store.Etcd) (map[string]strin
 	envKeys, err := etcd.List(envDirectoryKey, false)
 
 	if err != nil {
-		return nil, errors.Wrap(err, "Failed to get environment variable keys.")
+		return nil, err
 	}
 
 	for _, key := range envKeys {
 		value, err := etcd.Get(key)
 
 		if err != nil {
-			return nil, errors.Wrap(err, fmt.Sprintf("Failed to get environment variable value. key: %s", key))
+			return nil, err
 		}
 
 		envs[strings.Replace(key, envDirectoryKey, "", 1)] = value
@@ -134,7 +133,7 @@ func (app *Application) RegisterMetadata(etcd *store.Etcd) error {
 	}
 
 	if err := etcd.Set(appDirectoryKey+"/revisions/"+app.Revision, strconv.FormatInt(time.Now().Unix(), 10)); err != nil {
-		return errors.Wrap(err, "Failed to set revisdion.")
+		return err
 	}
 
 	return nil
