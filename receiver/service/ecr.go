e1327ec50b5134105ada813bcadda8521821ffb0
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-20 20:49:09 +0900

    Implement CreateRepository and RepositoryExists

--- receiver/service/ecr.go
+++ receiver/service/ecr.go
@@ -6,6 +6,20 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
+func CreateRepository(registryID, repository string) error {
+	svc := ecr.New(session.New(), &aws.Config{})
+
+	_, err := svc.CreateRepository(&ecr.CreateRepositoryInput{
+		RepositoryName: aws.String(repository),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func GetECRToken(registryID string) (string, error) {
 	svc := ecr.New(session.New(), &aws.Config{})
 
@@ -19,4 +33,24 @@ func GetECRToken(registryID string) (string, error) {
 	}
 
 	return *resp.AuthorizationData[0].AuthorizationToken, nil
+}
+
+func RepositoryExists(registryID, repository string) (bool, error) {
+	svc := ecr.New(session.New(), &aws.Config{})
+
+	resp, err := svc.DescribeRepositories(&ecr.DescribeRepositoriesInput{
+		RepositoryNames: []*string{
+			aws.String(repository),
+		},
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	if len(resp.Repositories) == 0 {
+		return false, nil
+	}
+
+	return true, nil
 }
