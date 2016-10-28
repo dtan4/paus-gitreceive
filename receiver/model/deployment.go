c8e848633c5e638ee6c0589db5b67507c34b1ada
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-28 16:34:23 +0900

    Add ServiceArn field

--- receiver/model/deployment.go
+++ receiver/model/deployment.go
@@ -15,8 +15,10 @@ type Deployment struct {
 	Branch      string
 	ProjectName string
 	Revision    string
+	ServiceArn  string
 }
 
+// DeploymentFromArgs creates new Deployment from gitreceive arguments
 // args:
 //   user/app, 19fb23cd71a4cf2eab00ad1a393e40de4ed61531, user, 4c:1f:92:b9:43:2b:23:0b:c0:e8:ab:12:cd:34:ef:56, refs/heads/branch-name
 func DeploymentFromArgs(app *Application, args []string) (*Deployment, error) {
@@ -27,11 +29,11 @@ func DeploymentFromArgs(app *Application, args []string) (*Deployment, error) {
 	revision := args[1]
 	branch := refnameRegexp.ReplaceAllString(args[4], "")
 
-	return NewDeployment(app, branch, revision), nil
+	return NewDeployment(app, branch, revision, ""), nil
 }
 
 // NewDeployment creates new Deployment object
-func NewDeployment(app *Application, branch, revision string) *Deployment {
+func NewDeployment(app *Application, branch, revision, serviceArn string) *Deployment {
 	projectName := app.Repository + "-" + revision[0:8]
 
 	return &Deployment{
@@ -39,5 +41,6 @@ func NewDeployment(app *Application, branch, revision string) *Deployment {
 		Branch:      branch,
 		ProjectName: projectName,
 		Revision:    revision,
+		ServiceArn:  serviceArn,
 	}
 }
